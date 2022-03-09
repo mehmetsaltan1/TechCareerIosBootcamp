481846c56c2aeb9ea428dabf6cdeabb959ce231c
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-03-09 20:12:51 +0300

    Detaylı Collection View projesi tamamlandı

---
+++ DetayliCollectionView/DetayliCollectionView/Filmler.swift
@@ -0,0 +1,27 @@
+//
+//  Filmler.swift
+//  DetayliCollectionView
+//
+//  Created by Mehmet Saltan on 9.03.2022.
+//
+
+import Foundation
+
+class Filmler {
+    var filmId:Int?
+    var filmAdi:String?
+    var filmResimAdi:String?
+    var filmYonetmenAdi:String?
+    var filmFiyat:Double?
+    
+    init(){
+        
+    }
+    init(filmId:Int,filmAdi:String,filmResimAdi:String,filmYonetmenAdi:String,filmFiyat:Double){
+        self.filmId = filmId
+        self.filmAdi = filmAdi
+        self.filmResimAdi = filmResimAdi
+        self.filmYonetmenAdi = filmYonetmenAdi
+        self.filmFiyat = filmFiyat
+    }
+}
---
+++ DetayliCollectionView/DetayliCollectionView/FilmlerCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FilmlerCollectionViewCell.swift
+//  DetayliCollectionView
+//
+//  Created by Mehmet Saltan on 9.03.2022.
+//
+
+import UIKit
+
+class FilmlerCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var filmImageView: UIImageView!
+    
+    @IBOutlet weak var filmAdiLabel: UILabel!
+    @IBOutlet weak var filmFiyatLabel: UILabel!
+    @IBAction func sepeteEkleButton(_ sender: Any) {
+    }
+}
