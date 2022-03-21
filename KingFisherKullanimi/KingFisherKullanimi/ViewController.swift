9836bf4333db2795c0b22d3d8db1c360bb798f50
Mehmet Saltan <44650542+mehmetsaltan1@users.noreply.github.com>
2022-03-21 20:16:41 +0300

    kingfisher kullanımı projesi tamamlandı

--- KingFisherKullanimi/KingFisherKullanimi/ViewController.swift
+++ KingFisherKullanimi/KingFisherKullanimi/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resimGoster(resimAd: "kofte.png")
     }
 
-
+    @IBAction func buttonResim1(_ sender: Any) {
+        resimGoster(resimAd: "fanta.png")
+    }
+    @IBAction func buttonResim2(_ sender: Any) {
+        resimGoster(resimAd: "ayran.png")
+    }
+    
+    func resimGoster(resimAd:String) {
+        if let url = URL(string: "http://kasimadalan.pe.hu/yemekler/resimler/\(resimAd)") {
+            DispatchQueue.main.async {
+                self.imageView.kf.setImage(with:url)
+            }
+        }
+    }
 }
 
